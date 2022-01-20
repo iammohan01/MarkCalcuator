6813484b5b8449640a47b01b9ce12fd70b856a44
ITSME-MOHAN <mohanasundaram7500@gmail.com>
2022-01-20 17:09:22 +0530

    confirmation

--- src/com/mark/percentage/Main.java
+++ src/com/mark/percentage/Main.java
@@ -11,6 +11,23 @@ public class Main {
         Scanner input = new Scanner(System.in);
         String nameOfPerson = input.next();
         System.out.println("Hello, " + nameOfPerson.toUpperCase());
-        
+
+        // No Of Paper
+        System.out.println("Enter No. Of papers : ");
+        byte noOfPapers = input.nextByte();
+        System.out.println(noOfPapers + "Papers . Is that correct ? If correct type " + ('"') + "yes" + ('"'));
+
+        //confirmation
+        String yes = "yes" ;
+        String getYes = input.next();
+        getYes = getYes.toUpperCase() ;
+        boolean confirmation = getYes == "YES" ;
+        System.out.println(getYes);
+        System.out.println(confirmation);
+
+
+
+
+
     }
 }
