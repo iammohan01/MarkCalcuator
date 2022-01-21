36e73497349a7c6fc71b51e23413f0bd5403ec55
ITSME-MOHAN <mohanasundaram7500@gmail.com>
2022-01-21 19:27:27 +0530

    Mark Cross Check
    Added If statement for marks cross chck

--- src/com/mark/percentage/Main.java
+++ src/com/mark/percentage/Main.java
@@ -6,7 +6,8 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// Welcome
+        // Welcome
+        String wrongMark = "You Entered Wrong Mark. Please restart the program ";
         System.out.println(" ____                                   __                                ____              ___                    ___             __                   \n" +
                 "/\\  _`\\                                /\\ \\__                            /\\  _`\\           /\\_ \\                  /\\_ \\           /\\ \\__                \n" +
                 "\\ \\ \\L\\ \\ __   _ __   ___     __    ___\\ \\ ,_\\    __       __      __    \\ \\ \\/\\_\\     __  \\//\\ \\     ___   __  __\\//\\ \\      __  \\ \\ ,_\\   ___   _ __  \n" +
@@ -17,120 +18,147 @@ public class Main {
                 "                                                           /\\____/                                                                                      \n" +
                 "                                                           \\_/__/                                                                                       ");
         System.out.println("Enter Your Name :");
+
+
+
         Scanner input = new Scanner(System.in);
-        String nameOfPerson = input.next();
+        String nameOfPerson = input.nextLine();
         System.out.println("Hello, " + nameOfPerson.toUpperCase());
 
         // No Of Paper
         System.out.println("Enter No. Of papers : ");
         short noOfPapers = input.nextShort();
-        System.out.println(noOfPapers + " Papers . Is that correct ? If correct, Type " + '"' +'1' + '"'+ ",  Else wrong , press any number ");
+        System.out.println(noOfPapers + " Papers . Is that correct ? If correct, Type " + '"' + '1' + '"' + ",  Else wrong , press any number ");
 
         //confirmation
         byte get1 = input.nextByte();
         String wrong;
         wrong = "You Entered Wrong No of paper . so please restart program ";
-        String confirmation = get1 != 1 ? wrong : "ok. Now Enter Full Mark Of Single Paper :"  ;
+        String confirmation = get1 != 1 ? wrong : "ok. Now Enter Full Mark Of Single Paper :";
         System.out.println(confirmation);
         //boolean = confirmation == "ok" ?  ;
 
         // Total Mark of single paper
         byte totalMarks1paper = input.nextByte();
-        int totalMarkOfAll = totalMarks1paper * noOfPapers ;
+        int totalMarkOfAll = totalMarks1paper * noOfPapers;
         System.out.println(totalMarkOfAll + " was your total marks . Now Enter Your 1st subject Name : ");
 
         // 1st Subject
         String sub1Name = input.next();
         sub1Name = sub1Name.toUpperCase();
-        System.out.println("Enter Your "+ sub1Name + " mark :");
+        System.out.println("Enter Your " + sub1Name + " mark :");
         byte sub1Mark = input.nextByte();
 
-        //System.out.println(sub1Name + " Mark = " + sub1Mark );
-        System.out.println("Now Enter 2nd Subject Name : ");
+
+        if (sub1Mark <= totalMarks1paper) {
+            System.out.println(sub1Name + " Mark = " + sub1Mark);
+            System.out.println("Now Enter 2nd Subject Name : ");
+        } else{
+            System.out.println(wrongMark);
+        }
 
         // 2nd Subject
         String sub2Name = input.next();
         sub2Name = sub2Name.toUpperCase();
-        System.out.println("Enter Your "+ sub2Name + " mark :");
+        System.out.println("Enter Your " + sub2Name + " mark :");
         byte sub2Mark = input.nextByte();
 
-        /*System.out.println(sub1Name + " Mark = " + sub1Mark );
-        System.out.println(sub2Name + " Mark = " + sub2Mark );*/
-
-        System.out.println("Now Enter 3rd Subject Name : ");
-
+        if (sub2Mark <= totalMarks1paper) {
+            System.out.println(sub1Name + " Mark = " + sub1Mark);
+            System.out.println(sub2Name + " Mark = " + sub2Mark);
+            System.out.println("Now Enter 3rd Subject Name : ");
+        } else{
+            System.out.println(wrongMark);
+        }
         // 3rd Subject
         String sub3Name = input.next();
         sub3Name = sub3Name.toUpperCase();
-        System.out.println("Enter Your "+ sub3Name + " mark :");
+        System.out.println("Enter Your " + sub3Name + " mark :");
         byte sub3Mark = input.nextByte();
 
-        /*System.out.println(sub1Name + " Mark = " + sub1Mark );
-        System.out.println(sub2Name + " Mark = " + sub2Mark );
-        System.out.println(sub3Name + " Mark = " + sub3Mark );*/
-        System.out.println("Now Enter 4th Subject Name  : ");
-
+        if (sub3Mark <= totalMarks1paper) {
+            System.out.println(sub1Name + " Mark = " + sub1Mark);
+            System.out.println(sub2Name + " Mark = " + sub2Mark);
+            System.out.println(sub3Name + " Mark = " + sub3Mark);
+            System.out.println("Now Enter 4th Subject Name : ");
+        } else {
+            System.out.println(wrongMark);
+        }
         // 4th subject
         String sub4Name = input.next();
         sub4Name = sub4Name.toUpperCase();
-        System.out.println("Enter Your "+ sub4Name + " mark :");
+        System.out.println("Enter Your " + sub4Name + " mark :");
         byte sub4Mark = input.nextByte();
 
-        /*System.out.println(sub1Name + " Mark = " + sub1Mark );
-        System.out.println(sub2Name + " Mark = " + sub2Mark );
-        System.out.println(sub3Name + " Mark = " + sub3Mark );
-        System.out.println(sub4Mark + " Mark = " + sub4Mark ); */
-        System.out.println("Now Enter 5th Subject Name :");
-
-        // 5 th Subject
-        String sub5Name = input.next();
-        sub5Name = sub5Name.toUpperCase();
-        System.out.println("Enter Your " + sub5Name + " mark :");
-        byte sub5Mark = input.nextByte();
-
-        /*System.out.println(sub1Name + " Mark = " + sub1Mark );
-        System.out.println(sub2Name + " Mark = " + sub2Mark );
-        System.out.println(sub3Name + " Mark = " + sub3Mark );
-        System.out.println(sub4Mark + " Mark = " + sub4Mark );
-        System.out.println(sub5Mark + " Mark = " + sub5Mark );*/
-        System.out.println("Now Enter 6th Subject Name :");
-
-        // 6th Subject
-        String sub6Name = input.next();
-        sub6Name = sub6Name.toUpperCase(Locale.ROOT);
-        System.out.println("Enter Your " + sub6Name + " mark : ");
-        byte sub6Mark = input.nextByte();
-
-       /* System.out.println(sub1Name + " Mark = " + sub1Mark );
-        System.out.println(sub2Name + " Mark = " + sub2Mark );
-        System.out.println(sub3Name + " Mark = " + sub3Mark );
-        System.out.println(sub4Name + " Mark = " + sub4Mark );
-        System.out.println(sub5Name + " Mark = " + sub5Mark );
-        System.out.println(sub6Name + " Mark = " + sub6Mark );*/
-        System.out.println("Now Enter 7th Subject Name : ");
-
-        // 7th Subject
-        String sub7Name = input.next();
-        sub7Name = sub7Name.toUpperCase();
-        System.out.println("Enter Your "+ sub7Name + " mark : ");
-        byte sub7Mark = input.nextByte();
+        if (sub4Mark <= totalMarks1paper) {
+            System.out.println(sub1Name + " Mark = " + sub1Mark);
+            System.out.println(sub2Name + " Mark = " + sub2Mark);
+            System.out.println(sub3Name + " Mark = " + sub3Mark);
+            System.out.println(sub4Mark + " Mark = " + sub4Mark);
+            System.out.println("Now Enter 5th Subject Name :");
+        } else {
+            System.out.println(wrongMark);}
 
 
-        System.out.println(sub1Name + " Mark = " + sub1Mark );
-        System.out.println(sub2Name + " Mark = " + sub2Mark );
-        System.out.println(sub3Name + " Mark = " + sub3Mark );
-        System.out.println(sub4Name + " Mark = " + sub4Mark );
-        System.out.println(sub5Name + " Mark = " + sub5Mark );
-        System.out.println(sub6Name + " Mark = " + sub6Mark );
-        System.out.println(sub7Name + " Mark + " + sub7Mark );
+            // 5 th Subject
+            String sub5Name = input.next();
+            sub5Name = sub5Name.toUpperCase();
+            System.out.println("Enter Your " + sub5Name + " mark :");
+            byte sub5Mark = input.nextByte();
 
-        //Total
-        int total= sub1Mark + sub2Mark + sub3Mark + sub4Mark + sub5Mark + sub6Mark + sub7Mark ;
-        int percentage = total/totalMarkOfAll*100 ;
-        System.out.println("Your ( " + nameOfPerson.toUpperCase() + " ) Total Marks = " + total );
-        System.out.println("Your ( " + nameOfPerson.toUpperCase() + " ) Percentage = " + percentage );
+            if (sub5Mark <= totalMarks1paper) {
+                System.out.println(sub1Name + " Mark = " + sub1Mark);
+                System.out.println(sub2Name + " Mark = " + sub2Mark);
+                System.out.println(sub3Name + " Mark = " + sub3Mark);
+                System.out.println(sub4Mark + " Mark = " + sub4Mark);
+                System.out.println(sub5Mark + " Mark = " + sub5Mark);
+                System.out.println("Now Enter 6th Subject Name :");
+            } else {
+                System.out.println(wrongMark);}
 
 
+                // 6th Subject
+                String sub6Name = input.next();
+                sub6Name = sub6Name.toUpperCase(Locale.ROOT);
+                System.out.println("Enter Your " + sub6Name + " mark : ");
+                byte sub6Mark = input.nextByte();
 
-    }
-}
+                if (sub6Mark <= totalMarks1paper) {
+                    System.out.println(sub1Name + " Mark = " + sub1Mark);
+                    System.out.println(sub2Name + " Mark = " + sub2Mark);
+                    System.out.println(sub3Name + " Mark = " + sub3Mark);
+                    System.out.println(sub4Name + " Mark = " + sub4Mark);
+                    System.out.println(sub5Name + " Mark = " + sub5Mark);
+                    System.out.println(sub6Name + " Mark = " + sub6Mark);
+                    System.out.println("Enter Your 7th Subject Name:; ");
+                } else {
+                    System.out.println(wrongMark);}
+
+                    // 7th Subject
+                    String sub7Name = input.next();
+                    sub7Name = sub7Name.toUpperCase();
+                    System.out.println("Enter Your " + sub7Name + " mark : ");
+                    byte sub7Mark = input.nextByte();
+
+                    if (sub7Mark <= totalMarks1paper) {
+                        System.out.println(sub1Name + " Mark = " + sub1Mark);
+                        System.out.println(sub2Name + " Mark = " + sub2Mark);
+                        System.out.println(sub3Name + " Mark = " + sub3Mark);
+                        System.out.println(sub4Name + " Mark = " + sub4Mark);
+                        System.out.println(sub5Name + " Mark = " + sub5Mark);
+                        System.out.println(sub6Name + " Mark = " + sub6Mark);
+                        System.out.println(sub7Name + " Mark + " + sub7Mark);
+                    } else {
+                        System.out.println(wrongMark);}
+
+
+                        //Total
+                        int total = sub1Mark + sub2Mark + sub3Mark + sub4Mark + sub5Mark + sub6Mark + sub7Mark;
+                        double percentage = (total / (noOfPapers * totalMarks1paper)) ;
+                        percentage *= 100 ;
+                        System.out.println("Your ( " + nameOfPerson.toUpperCase() + " ) Total Marks = " + total);
+                        System.out.println("Your ( " + nameOfPerson.toUpperCase() + " ) Percentage = " + percentage);
+
+
+                    }
+                }
